260bcf42ffd5d29f01dd985f16a5789a2c25cbb6
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-09 19:16:46 -0300

    01:07 - WeakSet

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var set = new Set();
-set.add(2);
-set.add(1);
-set.add(1);
-for (let item of set) {
-    console.log(item, 'daw');
-}
-set.delete(1);
-console.log(set.size);
-console.log(set.has(1));
-set.delete(1);
-console.log(set.has(1));
-console.log(set);
+const weakSet = new WeakSet();
+
+var obj = {};
+
+weakSet.add(obj);
+
+// console.log(weakSet.has(obj));
+
+obj = null;
+
+
+console.log(weakSet.has(obj));
