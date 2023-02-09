b7aa8e121d2bba0be59221138c2963580468ec40
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-09 19:15:32 -0300

    01:04 - Map vs Object

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const map = new Map();
-const meuObj = {};
 
-map.set('nome', 'Ariel');
-map.set('ano', 2023);
-map.set(meuObj, 'meu objeto');
-map.set(function () { }, 'minha função');
+const chamveObj = {};
 
-// map.delete('ano');
-// console.log(map.get('nome'));
+const obj = {
+    chamveObj: "Ariel"
+};
 
-// for (const [key, valor] of map) {
-//     console.log(`${key}:${valor}`);
-// }
-console.log(map.get(meuObj));
-console.log(map);
+map.set({}, 'meu objeto');
+
+console.log(obj.chamveObj);
+
+console.log('OBJ', obj);
+console.log('MAP', map);
