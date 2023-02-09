3c27d9093360cc95e8a4f5885a4bbd478d557099
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-09 19:15:14 -0300

    01:03 - Map

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const frutas = ["maçã", "banana", "laranja"];
+const map = new Map();
+const meuObj = {};
 
+map.set('nome', 'Ariel');
+map.set('ano', 2023);
+map.set(meuObj, 'meu objeto');
+map.set(function () { }, 'minha função');
 
-console.log(frutas[1]);
+// map.delete('ano');
+// console.log(map.get('nome'));
+
+// for (const [key, valor] of map) {
+//     console.log(`${key}:${valor}`);
+// }
+console.log(map.get(meuObj));
+console.log(map);
